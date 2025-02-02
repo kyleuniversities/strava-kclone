4aa9f7a34a5c70d17ec4882847c7a6efdc9e2afc
kyleuniversities <kyleuniversities@gmail.com>
2025-02-02 06:56:29 -0800

    added login page text tests

--- client/__tests__/LoginPromptPage.test.tsx
+++ client/__tests__/LoginPromptPage.test.tsx
@@ -5,7 +5,17 @@ import LoginPromptPage from "@/page/login-prompt/LoginPromptPage";
 describe("Login Page", () => {
   it("renders the title", () => {
     render(<LoginPromptPage />);
-    const titleElements = screen.getAllByText("Community-Powered Motivation");
-    expect(titleElements);
+    const elements = screen.getAllByText("Community-Powered Motivation");
+    expect(elements.length).toBeGreaterThan(0);
+  });
+  it("renders the sign up with email text", () => {
+    render(<LoginPromptPage />);
+    const elements = screen.getAllByText("Sign Up With Email");
+    expect(elements.length).toBeGreaterThan(0);
+  });
+  it("renders the Strava Logo twice", () => {
+    render(<LoginPromptPage />);
+    const elements = screen.getAllByText("STRAVA");
+    expect(elements.length).toEqual(2);
   });
 });
